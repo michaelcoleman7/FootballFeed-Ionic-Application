31f6638bd31fa701e4beb2831ecb6e715554bb04
Michael Coleman <Michaelcoleman135@gmail.com>
2018-04-26 20:49:56 +0100

    Added interpolation
    Added interpolation disabling the view table butoon until an option is selected

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+//import storage
 import { Storage } from '@ionic/storage';
+//import about page for navcontroller navigation
 import { AboutPage } from '../about/about';
 
 @IonicPage()
@@ -10,25 +12,32 @@ import { AboutPage } from '../about/about';
 })
 export class SettingsPage {
     LeagueChosen: any;
+    clicked: number;
+    buttonDisabled = true;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private storage:Storage) {
-   
+   //ensure button disabled on page load
+   this.RevealBtn(0);
 }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingsPage');
   }
-
+  //navigation to about page(league tables) sending with the league id chosen from radio selection
   update(){
     this.navCtrl.push(AboutPage, {
-       leagueValue: this.LeagueChosen
+      leagueValue: this.LeagueChosen
     });
   }
 
-  getData(){
-      this.storage.get('League_id').then((data) =>{
-      console.log(data);
-    }); 
+  //method to reveal button when an option is picked or disable if no option selected using interpolation
+  RevealBtn(input: number){
+    this.clicked = input;
+    if(this.clicked == 0){
+      this.buttonDisabled = true;
+    }
+    else{
+      this.buttonDisabled = false;
+    }
   }
-
 }
